2eb919cc6af54ca8882466f7f0bff5a3199e5c3b
kayalvizhi <madhukayal88@gmail.com>
2026-07-31 21:17:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
+import Greeting from './Greeting';
+import './styles.css';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app-container">
+      <h1>👋 Personalized Greeting</h1>
+      <p className="subtitle">Enter your name to see a personalized greeting</p>
+      <Greeting />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
